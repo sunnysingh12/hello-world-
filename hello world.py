3d0fd58197c50dafda27072a15539299348bb424
sunnysingh12 <32081776+sunnysingh12@users.noreply.github.com>
2017-10-21 14:53:53 +0100

    Add files via upload
    I created my file on Pythin and then uploaded it onto Github.

---
+++ hello world.py
@@ -0,0 +1,8 @@
+Python 3.6.2 (v3.6.2:5fd33b5926, Jul 16 2017, 20:11:06) 
+[GCC 4.2.1 (Apple Inc. build 5666) (dot 3)] on darwin
+Type "copyright", "credits" or "license()" for more information.
+>>> WARNING: The version of Tcl/Tk (8.5.9) in use may be unstable.
+Visit http://www.python.org/download/mac/tcltk/ for current information.
+print("hello world")
+hello world
+>>> 
